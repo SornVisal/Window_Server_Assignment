c36455d92611aee156738c7b9a5e4154db40d6a6
Visal Sorn <visal.prv@gmail.com>
2026-02-24 14:14:23 +0700

    Fix GroupsModule UsersService injection

--- backend/src/groups/groups.module.ts
+++ backend/src/groups/groups.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { SubmissionsModule } from '../submissions/submissions.module';
+import { UsersModule } from '../users/users.module';
 import { GroupsController } from './groups.controller';
 import { GroupsService } from './groups.service';
 
 @Module({
-  imports: [DatabaseModule, SubmissionsModule],
+  imports: [DatabaseModule, SubmissionsModule, UsersModule],
   controllers: [GroupsController],
   providers: [GroupsService],
 })
